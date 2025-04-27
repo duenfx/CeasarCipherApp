b5e1cc5f28c42d5ccbf63f94ec0a0a39ab95cfef
Rina <karinahanzha@gmail.com>
2025-04-27 17:44:52 +0300

    added readFile and writeFile methods

--- src/FileService.java
+++ src/FileService.java
@@ -1,3 +1,12 @@
-public class FileService {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class FileService {
+public static String readFile(Path path) throws IOException {
+    return Files.readString(path);
+}
+public static void writeFile(Path path, String text) throws IOException {
+    Files.writeString(path, text);
+}
 }
