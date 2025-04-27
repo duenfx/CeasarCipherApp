d9fa4be557b7fafd396bca7236340597759f0d4d
Rina <karinahanzha@gmail.com>
2025-04-27 21:49:32 +0300

    added encryption logic in EncryptService.java

--- src/EncryptService.java
+++ src/EncryptService.java
@@ -1,3 +1,28 @@
 public class EncryptService {
-
+    private static final char[] alphabet = new char[]
+    {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G',
+                'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                'V', 'W', 'X', 'Y', 'Z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z',
+                '.', ',', ':', '!', '?', '«', '»',
+                '"', ' '
+    };
+    public static String encryptText(char[] symbol, int key) {
+        String cipherText;
+        for (int i = 0; i < symbol.length; i++) {
+            for (int j = 0; j < alphabet.length; j++) {
+                if (symbol[i] == alphabet[j]) {
+                    symbol[i] = alphabet[(j + key) % alphabet.length];
+                    break;
+                }
+            }
+        }
+        cipherText = new String(symbol);
+        return cipherText;
+    }
 }
