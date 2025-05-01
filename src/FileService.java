368878a1d1a4b5b271aa10a547d62889fd92c3d7
Rina <karinahanzha@gmail.com>
2025-05-01 14:05:10 +0300

    added new logic in FileService.java

--- src/FileService.java
+++ src/FileService.java
@@ -7,7 +7,27 @@ public class FileService {
         return Files.readString(path);
     }
 
-    public static void writeFile(Path path, String text) throws IOException {
-        Files.writeString(path, text);
+    public static void writeFile(Path path, String text, String mode) throws IOException {
+        String fileName = path.getFileName().toString();
+        int index = fileName.lastIndexOf(".");
+        String name;
+        String extension;
+        if (index != -1) {
+            name = fileName.substring(0, index);
+            extension = fileName.substring(index);
+        } else {
+            name = fileName;
+            extension = "";
+        }
+        String suffix = "";
+        if (mode.equalsIgnoreCase("ENCRYPT")) {
+            suffix = "[ENCRYPTED]";
+        } else if (mode.equalsIgnoreCase("DECRYPT")) {
+            suffix = "[DECRYPTED]";
+        }
+        String newFileName = name + suffix + extension;
+        Path dir = path.getParent();
+        Path newPath = dir.resolve(newFileName);
+        Files.writeString(newPath, text);
     }
 }
